58f630ede0995ee13cbc20b120965cd244bfeb54
VastoLordePy <vastolordepy@gmail.com>
2022-07-09 13:51:37 -0400

    Added comments

--- lua/efe.lua
+++ lua/efe.lua
@@ -1,35 +1,33 @@
 local fn = vim.fn
 
-local function efeEnter()
-    --vim.api.nvim_set_hl(0, 'LineNr', { bg = "#373b41", fg = "NONE" })
-    vim.cmd('hi CursorLineNr guibg=#373b41 guifg=#373b41 | hi LineNr guibg=#373b41')
-    vim.cmd("redraw")
-end
-
-local function efeLeave()
-    --vim.api.nvim_set_hl(0, 'LineNr', { bg = "NONE", fg = "#373b41" })
-    vim.cmd("hi LineNr guibg=NONE | hi CursorLineNR guibg=NONE guifg=#5f87af")
-end
+--global variable that holds the target of the previous search 
 vim.g.efePreviousTarget = ''
+
+--Repeats the last search in the forward direction.
+--the cursor_pos holds a table in which the first element is the number of the current line.
+--this number is used to restrict the search to the current line the cursor is in.
 local function repeat_last_efe_forward()
     local target = vim.g.efePreviousTarget
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
     fn.search(target, '', cursor_pos[1])
 end
 
+--Same as the forward function but backwards
 local function repeat_last_efe_backward()
     local target = vim.g.efePreviousTarget
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
     fn.search(target, 'b', cursor_pos[1])
 end
 
+--Takes two characters as input, and in the current line and after the cursor, 
+--searches for the first occurrence of the inputted text. If an occurrence is found
+--moves the cursor to the first character of the target text, if no occurrence is found
+--does nothing
 local function efe_forward()
-    --efeEnter()
     local current_line = vim.api.nvim_get_current_line()
     local first_char = fn.getcharstr()
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
     if (string.byte(first_char) == 27) then
-        --efeLeave()
         return
     end
     local second_char = fn.getcharstr()
@@ -37,20 +35,17 @@ local function efe_forward()
         string.sub(current_line, #current_line) == first_char)
     then
         vim.api.nvim_win_set_cursor(0, {cursor_pos[1], #current_line - 1})
-        --efeLeave()
         return
     end
     local target = first_char .. second_char
     vim.g.efePreviousTarget = target
     fn.search(target, '', cursor_pos[1])
-    --efeLeave()
 end
 
+--Same as efe_forward() but backwards
 local function efe_backward()
-    --efeEnter()
     local first_char = fn.getcharstr()
     if (string.byte(first_char) == 27) then
-        --efeLeave()
         return
     end
     local second_char = fn.getcharstr()
@@ -58,12 +53,11 @@ local function efe_backward()
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
     vim.g.efePreviousTarget = target
     fn.searchpos(target, 'b', cursor_pos[1])
-
-    --efeLeave()
 end
 
 
 
+--mappings
 local function efe()
     vim.keymap.set(
         { 'n', 'v', 'o' },
@@ -71,7 +65,6 @@ local function efe()
         ':lua require"efe".efe_forward()<CR>',
         { silent = true }
     )
-    --vim.keymap.set({ 'n', 'v', 'o' }, 'h', ':lua require"efe".efe_backward()<CR>')
     vim.keymap.set(
         { 'n', 'v', 'o' },
         'h',
